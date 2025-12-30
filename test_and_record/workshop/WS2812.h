99225bb1dffbf3cf832cf9a515065da0f9d81241
Rob Bothof <rbothof@xs4all.nl>
2025-12-30 16:36:45 +0100

    update ws2812 lib

--- test_and_record/workshop/WS2812.h
+++ test_and_record/workshop/WS2812.h
@@ -89,6 +89,77 @@ inline const Color Color::Orange = Color(255, 165, 0);
 inline const Color Color::Purple = Color(128, 0, 128);
 inline const Color Color::Pink = Color(255, 192, 203);
 
+class LEDBuildin {
+private:
+  bool initialized;
+  uint8_t pin;
+
+  public:
+  LEDBuildin() : pin(0), initialized(false) {}
+
+  Color led;  
+
+  void init(uint8_t datapin) {
+    pin = datapin;
+    #if SOC_RMT_SUPPORTED
+
+    if (!rmtInit(pin, RMT_TX_MODE, RMT_MEM_NUM_BLOCKS_1, 10000000)) {
+      Serial.println("RMT initialization failed!");
+      return;
+    }
+    initialized=true;
+    #endif
+  }
+  
+  void update() {
+    if (!initialized) return;
+    
+    #if SOC_RMT_SUPPORTED
+  
+    rmt_data_t led_data[24];
+    int led_data_index = 0;
+    uint8_t led_data_value=0;
+
+      for (int c = 0; c < 3; c++) {
+        for (int bit = 0; bit < 8; bit++) {
+          if (c==1) led_data_value = led.r;
+          if (c==0) led_data_value = led.g;
+          if (c==2) led_data_value = led.b;
+
+          if (led_data_value & (1 << (7 - bit))) {
+            led_data[led_data_index].level0 = 1; // HIGH bit
+            led_data[led_data_index].duration0 = 8; // 0.8us
+            led_data[led_data_index].level1 = 0; // LOW bit
+            led_data[led_data_index].duration1 = 4; // 0.4us
+          } else {
+            led_data[led_data_index].level0 = 1; // LOW bit
+            led_data[led_data_index].duration0 = 4; // 0.4us
+            led_data[led_data_index].level1 = 0; // HIGH bit
+            led_data[led_data_index].duration1 = 8; // 0.8us
+          }
+          led_data_index++;
+        }
+      }
+    
+
+    // Write the data to the LEDRING
+    rmtWrite(pin, led_data, RMT_SYMBOLS_OF(led_data), RMT_WAIT_FOR_EVER);
+
+    #else
+    Serial.println("RMT is not supported on this target.");
+    #endif
+  }
+  
+  void clear() {
+    led.hex=0x000000;
+  }
+
+  void setColor(Color color) {
+    led = color;
+  }
+
+};
+
 class WS2812 {
 private:
   uint16_t numLeds;
